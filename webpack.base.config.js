5d218b73b440d2e1a27980439ef91988373b6506
Daniel Friedman <dfriedman58@gmail.com>
2017-04-06 17:39:47 -0400

    More messing with babel config

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: ["babel-polyfill", "./src/index.js"],
+  entry: ["./src/index.js"],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
