9761d5d210a27379c80da6e1bc77d1a1170ba176
Daniel Friedman <dfriedman58@gmail.com>
2017-04-19 21:28:26 -0400

    Add node spec

---
+++ src/rdf/node.spec.js
@@ -0,0 +1,39 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+
+import Immutable from 'immutable'
+
+import { Node, nodeSet } from './node'
+
+describe('node', () => {
+  describe('Node', () => {
+    it('constructs an immutable record', () => {
+      expect(Node()).to.be.an.instanceof(Immutable.Record)
+    })
+
+    it('creates default fields for `termType`, `value`, `language`, and `datatype`', () => {
+      const node = Node()
+      expect(node.termType).to.be.null
+      expect(node.value).to.be.null
+      expect(node.language).to.be.null
+      expect(node.datatype).to.be.null
+      expect(node.notRealField).to.be.undefined
+    })
+  })
+
+  describe('nodeSet', () => {
+    it('creates an immutable set', () => {
+      expect(nodeSet()).to.be.an.instanceof(Immutable.Set)
+    })
+
+    it('converts its iterable argument to Nodes', () => {
+      const ns = nodeSet([
+        { termType: 'NamedNode', value: 'https://example.com/' }
+      ])
+      const first = ns.first()
+      expect(first).to.be.an.instanceof(Immutable.Record)
+      expect(first.termType).to.equal('NamedNode')
+      expect(first.value).to.equal('https://example.com/')
+    })
+  })
+})
